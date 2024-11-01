d44a483783ef947c397b5af41a143d69c7aed4c1
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-11-01 20:22:15 +0100

    fix image

--- app/components/UI/imageServer/imageServer.tsx
+++ app/components/UI/imageServer/imageServer.tsx
@@ -1,27 +1,18 @@
-"use client";
-
 import Image from "next/image";
 import { backHost } from "@/app/utils/server/server";
-import { useState } from "react";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
 
 const ImageServer = ({ link, width, height }: { link: string, width: number, height: number }) => {
-  const [loading, setLoading] = useState(true)
 
   return (
-    <div>
+    <>
       <Image
         fetchPriority="high"
         src={backHost + link}
         width={width}
         height={height}
         alt={"link"}
-        onLoadingComplete={() => { setLoading(false) }}
       />
-      <Skeleton />
-
-    </div>
+    </>
   );
 };
 
