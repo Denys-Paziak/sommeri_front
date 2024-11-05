32243c1422edfa31f549c3297d14728cf69918b6
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-11-05 06:14:07 +0100

    fix sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,12 +6,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let posts = await getProjects("en");
 
   const postUrls = posts.map((post: ProjectInterface) => ({
-    url: `http://test.com/uk/project/${post.url}`,
+    url: `https://sommeri.com/uk/project/${post.url}`,
     lastModified: new Date(),
     alternates: {
       languages: {
-        uk: `http://test.com/uk/project/${post.url}`,
-        en: `http://test.com/en/project/${post.url}`,
+        uk: `https://sommeri.com/uk/project/${post.url}`,
+        en: `https://sommeri.com/en/project/${post.url}`,
       },
     },
     priority: "0.8",
@@ -19,35 +19,35 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: "http://test.com",
+      url: "https://sommeri.com",
       lastModified: new Date(),
       alternates: {
         languages: {
-          uk: "http://test.com/uk",
-          en: "http://test.com/en",
+          uk: "https://sommeri.com/uk",
+          en: "https://sommeri.com/en",
         },
       },
       priority: "1.00",
     },
     ...postUrls,
     {
-      url: "http://test.com/uk/privacy-policy",
+      url: "https://sommeri.com/uk/privacy-policy",
       lastModified: new Date(),
       alternates: {
         languages: {
-          uk: "http://test.com/uk/privacy-policy",
-          en: "http://test.com/en/privacy-policy",
+          uk: "https://sommeri.com/uk/privacy-policy",
+          en: "https://sommeri.com/en/privacy-policy",
         },
       },
       priority: "0.7",
     },
     {
-      url: "http://test.com/uk/terms-of-use",
+      url: "https://sommeri.com/uk/terms-of-use",
       lastModified: new Date(),
       alternates: {
         languages: {
-          uk: "http://test.com/uk/terms-of-use",
-          en: "http://test.com/en/terms-of-use",
+          uk: "https://sommeri.com/uk/terms-of-use",
+          en: "https://sommeri.com/en/terms-of-use",
         },
       },
       priority: "0.7",
