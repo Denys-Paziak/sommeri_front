c04f8ce0a0c7dd4ff96469238a44a9d1c912238a
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-11-12 11:12:52 +0100

    pull

--- app/components/global/logo/Logo.tsx
+++ app/components/global/logo/Logo.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-
 const Logo = ({ theme }: { theme: string | undefined }) => {
+
+  if (!theme) {
+    return <Link href={"/"}><Image
+      width={300}
+      height={100}
+      src={`/images/light_logo.svg`}
+      alt="logo"
+    /></Link >;
+  }
+
+
   return (
     <Link href={"/"}>
-      {
-        theme != undefined && <Image
-          width={300}
-          height={100}
-          src={`../../images/${theme}_logo.svg`}
-          alt="logo"
-        />
-      }
-
+      <Image
+        width={300}
+        height={100}
+        src={`/images/${theme}_logo.svg`}
+        alt="logo"
+      />
     </Link>
   );
 };
